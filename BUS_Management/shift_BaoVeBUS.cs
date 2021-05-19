662dc134c9f28fa8cf457386e272d3da09e76b0b
Hieu Dan <hieudankt@gmail.com>
2021-05-19 09:40:37 +0700

    chinh id Nhan Vien

--- BUS_Management/shift_BaoVeBUS.cs
+++ BUS_Management/shift_BaoVeBUS.cs
@@ -48,7 +48,7 @@ namespace BUS_Management
                 {
                     for (int k = 0; k < Result[i][j].Count; k++)//slnvtc
                     {
-                        string id_NV = table.Rows[Result[i][j][k]]["id"].ToString();
+                        string id_NV = table.Rows[Result[i][j][k]]["ID CCCD"].ToString();
                         shift_BaoVeDTO shift_BaoVeDTO = dto(id_NV, thu, ca);
                         shift_baoveDAL.insertShift(shift_BaoVeDTO);
                     }
